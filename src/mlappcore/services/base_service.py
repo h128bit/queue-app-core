39683727793d7fb553b87ffa6a20ea040e5f6db0
Асмус Тимофей Андреевич <t.asmus@fgau.local>
2026-05-21 09:22:08 +0300

    fix err in BaseServiseAsync

--- src/mlappcore/services/base_service.py
+++ src/mlappcore/services/base_service.py
@@ -19,6 +19,41 @@ class BaseService:
                  method: Callable[[Any], bytes]|None=None):
         self._logger = logging.getLogger(self.__class__.__name__)
 
+        q_config, db_config = self._read_config(config_path)
+
+        # config = dotenv_values(config_path)
+
+        # q_url = config["QUEUE_URL"]
+        # q_user = config["QUEUE_USER"]
+        # q_pass = config["QUEUE_PASSWORD"]
+        # q_port = int(config["QUEUE_PORT"])
+
+        # obj_url = config["OBJECT_DB_PATH"]
+
+        # self._request_q = request_q
+        # self._response_q = response_q
+
+        # config = {
+        #     "url": q_url,
+        #     "user": q_user,
+        #     "password": q_pass,
+        #     "port": q_port,
+        #     "q_name": request_q 
+        # }
+
+        q_config["q_name"] = request_q
+        match handler_type:
+            case "router":
+                self._q_broker = RabbitPublisher(**q_config)
+            case "handler":
+                self._q_broker = RabbitConsumer(on_message=method, **q_config)
+            case _:
+                raise ValueError(f"Unsupported handler type, expected `router` or `handler`, got {handler_type}")
+            
+        self._obj_db_client = SimpleSeaweedPublisherConsumer(query_q=request_q, 
+                                                             response_q=response_q,
+                                                             *db_config)
+    def _read_config(self, config_path: str):
         config = dotenv_values(config_path)
 
         q_url = config["QUEUE_URL"]
@@ -28,29 +63,20 @@ class BaseService:
 
         obj_url = config["OBJECT_DB_PATH"]
 
-        self._request_q = request_q
-        self._response_q = response_q
-
-        config = {
+        queue_config = {
             "url": q_url,
             "user": q_user,
             "password": q_pass,
             "port": q_port,
-            "q_name": request_q 
         }
 
-        match handler_type:
-            case "router":
-                self._q_broker = RabbitPublisher(**config)
-            case "handler":
-                self._q_broker = RabbitConsumer(on_message=method, **config)
-            case _:
-                raise ValueError(f"Unsupported handler type, expected `router` or `handler`, got {handler_type}")
-            
-        self._obj_db_client = SimpleSeaweedPublisherConsumer(obj_url, 
-                                                             query_q=request_q, 
-                                                             response_q=response_q)
-        
+        object_db_config = {
+            "object_db_path_or_url": obj_url
+        }
+
+        return queue_config, object_db_config
+
+
 
 class BaseServiceAsync(BaseService):
     def __init__(self, 
@@ -66,7 +92,7 @@ class BaseServiceAsync(BaseService):
                          method=method)
         
         if handler_type == "router":
-            config = dotenv_values(config_path)
-            config.pop("OBJECT_DB_PATH", None)
-            self._q_broker = RabbitPublisherAsync(**config)
+            q_config, _ = self._read_config(config_path)
+            q_config["q_name"] = request_q
+            self._q_broker = RabbitPublisherAsync(**q_config)
                 
